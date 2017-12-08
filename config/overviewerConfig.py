095de45d5439883bfbf6de2679faeca3391ffcc0
Tyler Hallada <tyler@hallada.net>
2017-12-08 10:30:18 -0500

    Changing config symlinks to hard links w/ content

--- config/overviewerConfig.py
+++ config/overviewerConfig.py
@@ -1,1 +1,130 @@
-/srv/minecraft-panic-shack/overviewerConfig.py
+worlds["Panic Shack"] = "/srv/minecraft-panic-shack/world"
+
+renders["normalrender"] = {
+    "world": "Panic Shack",
+    "title": "North",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection": "upper-left",
+}
+
+renders["normalrendersouth"] = {
+    "world": "Panic Shack",
+    "title": "South",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection": "lower-right",
+}
+
+renders["normalrenderwest"] = {
+    "world": "Panic Shack",
+    "title": "West",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection": "upper-right",
+}
+
+renders["normalrendereast"] = {
+    "world": "Panic Shack",
+    "title": "East",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection": "lower-left",
+}
+
+renders["nightrender"] = {
+    "world": "Panic Shack",
+    "title": "Night North",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+    "northdirection": "upper-left",
+}
+
+renders["nightrendersouth"] = {
+    "world": "Panic Shack",
+    "title": "Night South",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+    "northdirection": "lower-right",
+}
+
+renders["nightrenderwest"] = {
+    "world": "Panic Shack",
+    "title": "Night West",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+    "northdirection": "upper-right",
+}
+
+renders["nightrendereast"] = {
+    "world": "Panic Shack",
+    "title": "Night East",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+    "northdirection": "lower-left",
+}
+
+renders["caverender"] = {
+    "world": "Panic Shack",
+    "title": "Cave North",
+    "rendermode": cave,
+    "dimension": "overworld",
+    "northdirection": "upper-left",
+}
+
+renders["caverendersouth"] = {
+    "world": "Panic Shack",
+    "title": "Cave South",
+    "rendermode": cave,
+    "dimension": "overworld",
+    "northdirection": "lower-right",
+}
+
+renders["caverenderwest"] = {
+    "world": "Panic Shack",
+    "title": "Cave West",
+    "rendermode": cave,
+    "dimension": "overworld",
+    "northdirection": "upper-right",
+}
+
+renders["caverendereast"] = {
+    "world": "Panic Shack",
+    "title": "Cave East",
+    "rendermode": cave,
+    "dimension": "overworld",
+    "northdirection": "lower-left",
+}
+
+renders["netherrender"] = {
+    "world": "Panic Shack",
+    "title": "Nether North",
+    "rendermode": nether,
+    "dimension": "nether",
+    "northdirection": "upper-left",
+}
+
+renders["netherrendersouth"] = {
+    "world": "Panic Shack",
+    "title": "Nether South",
+    "rendermode": nether,
+    "dimension": "nether",
+    "northdirection": "lower-right",
+}
+
+renders["netherrenderwest"] = {
+    "world": "Panic Shack",
+    "title": "Nether West",
+    "rendermode": nether,
+    "dimension": "nether",
+    "northdirection": "upper-right",
+}
+renders["netherrendereast"] = {
+    "world": "Panic Shack",
+    "title": "Nether East",
+    "rendermode": nether,
+    "dimension": "nether",
+    "northdirection": "lower-left",
+}
+
+outputdir = "/var/www/panic-shack/map/"
